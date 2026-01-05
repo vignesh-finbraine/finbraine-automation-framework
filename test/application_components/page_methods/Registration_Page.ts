8185e6e5a40a3d5e005881ff1a9d444029412587
automatet <himanshu.shar@newvision-software.com>
2026-01-05 12:39:15 +0530

    Update Registration_Page.ts

--- test/application_components/page_methods/Registration_Page.ts
+++ test/application_components/page_methods/Registration_Page.ts
@@ -335,43 +335,7 @@ await expect(results.first()).toBeVisible();
     await this.page.waitForTimeout(5000);
     await expect(this.Search_name).toBeVisible();
     await this.playwrightFactory.click(this.Search_name)
-
     }
-    async user_enters_edit_reg_ammount(stramount: string){
-      await this.page.waitForTimeout(5000);
-      await this.Registration_fees.clear();
-      await this.playwrightFactory.fill(this.Registration_fees,stramount)
-      }
-  async user_enters_edit_fund_ammount(stramount: string){
-        await this.page.waitForTimeout(5000);
-        await this.Fundraising_fees.clear();
-        await this.page.waitForTimeout(5000);
-        await this.playwrightFactory.fill(this.Fundraising_fees,stramount)
- 
-}
-async verify_edit_reg_autopopulated_fields(){
-        await expect (this.Event_tittle).toBeVisible();
-        await expect (this.Payment_opn).toBeVisible();
-       
-     }
-     async verify_edit_reg_checkbox_fields(){
-      await expect (this.published_box).toBeVisible();
-      await expect (this.Tick_box).toBeVisible();
-     }
-     async user_click_wolfcity(strEvent: string){
-      await this.playwrightFactory.click(this.page.locator("//div[contains(text(),'"+strEvent+"')]"));
-    }
-    async user_clicks_edit_btn_of_registration_page(strPage:string){
-      await this.page.locator("//span[contains(text(),'"+strPage+"')]").hover();
-      await this.playwrightFactory.click(this.page.locator("//span[contains(text(),'"+strPage+"')]"));
-      await this.page.waitForTimeout(5000)
-    }
-  async user_clicks_registration_fee_checkbox(){
-
-  }
-
-    }
-
   async user_enters_edit_reg_ammount(stramount: string) {
       await this.page.waitForTimeout(5000);
       await this.Registration_fees.clear();
@@ -404,7 +368,6 @@ async verify_edit_reg_autopopulated_fields(){
       await this.page.waitForTimeout(5000)
     }
   async user_clicks_registration_fee_checkbox() {
-
     await this.playwrightFactory.click(this.checkbox_registration_fee);
   }
  
@@ -526,4 +489,3 @@ async verify_edit_reg_autopopulated_fields(){
       }
  
 }
-
